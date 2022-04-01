964d4566c2a4541b6a0a64fd39ee29ebba9e4b06
alasantos <aluiz.santos@gmail.com>
2022-03-31 21:06:09 -0300

    Registered models Category and Itens

--- auctions/models.py
+++ auctions/models.py
@@ -9,19 +9,22 @@ class User(AbstractUser):
     pass
 
 class Category( models.Model ):
-    Name = models.CharField(name='categoryName', max_length=64)
-    Description = models.TextField(name='categoryDescription')
-    CreateDate = models.DateField(name='categoryCreateDate',default=date.today)
+    name = models.CharField(name='Name', max_length=64)
+    description = models.TextField(name='Description')
+    CreateDate = models.DateField(name='Createdate',default=date.today)
    # User = models.ForeignKey( User, on_delete=CASCADE)
 
     def __str__(self):
-        return f"{self.categoryName} : {self.categoryDescription} ({self.categoryCreateDate})"
+        return f"{self.Name} : {self.Description} ({self.Createdate})"
         
 class Item( models.Model ):
-    ShortDescription = models.CharField( name='ShortDescription', max_length=64)
-    LongDescription = models.TextField( name='CompleteDescription')
-    CategoryId = models.ForeignKey( Category, name='Category', null=TRUE ,on_delete=models.SET_NULL, related_name="ItensPerCategory")
-    UserId = models.ForeignKey( User, name="User", on_delete= models.CASCADE, related_name="ItemsPerCategory", default=1)
+    title = models.CharField( name='Title', max_length=64)
+    description = models.TextField( name='Description')
+    ItemimageURL = models.CharField( name='ItemImage', max_length=128)
+    startingBid = models.DecimalField( name='StartingBid', max_digits=8, decimal_places=2)
+    categoryId = models.ForeignKey( Category, name='Category', null=True ,on_delete=models.SET_NULL, related_name="ItensPerCategory")
+    userId = models.ForeignKey( User, name="User", on_delete= models.CASCADE, related_name="ItemsPerCategory", default=1)
+    createDate = models.DateField(name="CreateDate", default=date.today)
 
-    def __str__ ( self ):
-        return f"Item description: {self.ShortDescription} - CategoryId: {self.Category}"
+    def __str__( self ):
+        return f"({self.id}) - {self.Title} (price {self.startingBid}) "
