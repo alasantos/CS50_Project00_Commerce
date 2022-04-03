edfbc1b5e6687aedd58b53296186e7736f578135
alasantos <aluiz.santos@gmail.com>
2022-04-02 21:51:12 -0300

    twiking the auctions model

--- auctions/models.py
+++ auctions/models.py
@@ -21,11 +21,11 @@ class Item( models.Model ):
     id_item = models.AutoField( name='id_item', primary_key=True)
     title = models.CharField( name='Title', max_length=64)
     description = models.TextField( name='Description')
-    ItemimageURL = models.CharField( name='ItemImage', max_length=128)
-    startingBid = models.DecimalField( name='StartingBid', max_digits=8, decimal_places=2)
+    ItemimageURL = models.URLField( name='ItemImage', max_length=128)
+    startingBid = models.DecimalField( name='startingBid', max_digits=8, decimal_places=2)
     categoryId = models.ForeignKey( Category, name='Category', null=True ,on_delete=models.SET_NULL, related_name="ItensPerCategory")
     userId = models.ForeignKey( User, name="User", on_delete= models.CASCADE, related_name="ItemsPerCategory", default=1)
     createDate = models.DateField(name="CreateDate", default=date.today)
 
     def __str__( self ):
-        return f"({self.id}) - {self.Title} (price {self.startingBid}) "
+        return f"({self.id_item}) - {self.Title} (price {self.startingBid}) "
